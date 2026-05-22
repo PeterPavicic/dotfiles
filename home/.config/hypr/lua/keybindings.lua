1dd456fb0fadca8ca76f29cae69f10fd6814d67d
Peter Pavicic <peter.pavicic@gmail.com>
2026-05-22 21:18:49 +0200

    Hypr - FEAT: media control

--- home/.config/hypr/lua/keybindings.lua
+++ home/.config/hypr/lua/keybindings.lua
@@ -122,7 +122,12 @@ hl.bind(
 hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
 hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true })
 
--- Requires playerctl
+-- Media control through playerctl
 hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
 hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
+
+-- alternatives using Pause, Scroll_Lock
+hl.bind("Pause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+hl.bind("Scroll_Lock", hl.dsp.exec_cmd("playerctl next"), { locked = true })
+hl.bind("SHIFT + Scroll_Lock", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
